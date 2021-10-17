52ada58e86132c01edcd09c0a2f6d720dc6b9517
Kirit Sælensminde <kirit@kirit.com>
2021-10-17 14:09:44 +0700

    Start coroutines and run one until completion

--- include/felspar/poll/executor.hpp
+++ include/felspar/poll/executor.hpp
@@ -3,18 +3,36 @@
 
 #include <felspar/coro/task.hpp>
 
+#include <vector>
+
 
 namespace felspar::poll {
 
 
     /// Executor that allows `poll` based asynchronous IO
     class executor {
+        std::vector<
+                felspar::coro::unique_handle<felspar::coro::task_promise<void>>>
+                live;
+
       public:
         template<typename... PArgs, typename... MArgs>
         void
                 post(coro::task<void> (*f)(executor &, PArgs...),
                      MArgs &&...margs) {
-            auto coro = f(*this, std::forward<MArgs>(margs)...);
+            auto task = f(*this, std::forward<MArgs>(margs)...);
+            auto coro = task.release();
+            coro.resume();
+            live.push_back(std::move(coro));
+        }
+        template<typename... PArgs, typename... MArgs>
+        void run(coro::task<void> (*f)(executor &, PArgs...), MArgs &&...margs) {
+            auto task = f(*this, std::forward<MArgs>(margs)...);
+            auto coro = task.release();
+            coro.resume();
+            while (not coro.done()) {
+                /// Do poll here....
+            }
         }
     };
 
--- test/run/basics.cpp
+++ test/run/basics.cpp
@@ -1,5 +1,10 @@
+#include <felspar/exceptions.hpp>
 #include <felspar/poll.hpp>
 #include <felspar/test.hpp>
+
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 
 namespace {
@@ -10,12 +15,41 @@ namespace {
 
     felspar::coro::task<void>
             echo_server(felspar::poll::executor &exec, std::uint16_t port) {
+        auto fd = ::socket(AF_INET, SOCK_STREAM, 0);
+        if (fd < 0) {
+            throw felspar::stdexcept::system_error{
+                    errno, std::generic_category(), "Creating server socket"};
+        }
+
+        sockaddr_in in;
+        in.sin_family = AF_INET;
+        in.sin_port = htons(port);
+        in.sin_addr.s_addr = htonl(INADDR_ANY);
+        if (::bind(fd, reinterpret_cast<sockaddr const *>(&in), sizeof(in))
+            != 0) {
+            throw felspar::stdexcept::system_error{
+                    errno, std::generic_category(), "Binding server socket"};
+        }
+
+        ::close(fd);
+        co_return;
+    }
+
+    felspar::coro::task<void>
+            echo_client(felspar::poll::executor &exec, std::uint16_t port) {
+        auto fd = ::socket(AF_INET, SOCK_STREAM, 0);
+        if (fd < 0) {
+            throw felspar::stdexcept::system_error{
+                    errno, std::generic_category(), "Creating client socket"};
+        }
+
         co_return;
     }
 
     auto const trans = suite.test("transfer", []() {
         felspar::poll::executor exec;
         exec.post(echo_server, 5543);
+        exec.run(echo_client, 5543);
     });
 
 
