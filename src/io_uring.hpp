a16aee96ec95b0f0f07e4317de266a719d528138
Kirit Sælensminde <kirit@kirit.com>
2022-01-22 17:06:07 +0700

    Change timing so the underlying IOP isn't created until the co_await is hit

--- src/io_uring.hpp
+++ src/io_uring.hpp
@@ -10,16 +10,25 @@ namespace felspar::poll {
 
 
     struct io_uring_warden::completion : public poll::completion {
-        completion(io_uring_warden *w) : self{w} {}
+        completion(io_uring_warden *w, void (*o)(completion *))
+        : self{w}, order_iop{o} {}
+
+        io_uring_warden *self;
+        void (*order_iop)(completion *);
 
         warden *ward() override { return self; }
         void await_suspend(felspar::coro::coroutine_handle<> h) override {
             handle = h;
+            order_iop(this);
         }
 
-        io_uring_warden *self;
+        /// Extra storage that can be used for "stuff" important to IOPs
+        int fd = {};
         sockaddr addr = {};
+        sockaddr const *addrptr = {};
         socklen_t addrlen = {};
+        std::span<std::byte> bytes = {};
+        std::span<std::byte const> cbytes = {};
 
         static void execute(::io_uring *, ::io_uring_cqe *);
     };
--- src/iops.io_uring.cpp
+++ src/iops.io_uring.cpp
@@ -7,28 +7,41 @@
 
 felspar::poll::iop<std::size_t> felspar::poll::io_uring_warden::read_some(
         int fd, std::span<std::byte> b, felspar::source_location loc) {
-    auto sqe = ring->next_sqe();
-    auto *c = new completion{this};
-    ::io_uring_prep_read(sqe, fd, b.data(), b.size(), 0);
-    ::io_uring_sqe_set_data(sqe, c);
+    auto *c = new completion{this, [](completion *c) {
+                                 auto sqe = c->self->ring->next_sqe();
+                                 ::io_uring_prep_read(
+                                         sqe, c->fd, c->bytes.data(),
+                                         c->bytes.size(), 0);
+                                 ::io_uring_sqe_set_data(sqe, c);
+                             }};
+    c->fd = fd;
+    c->bytes = b;
     return {c};
 }
 felspar::poll::iop<std::size_t> felspar::poll::io_uring_warden::write_some(
         int fd, std::span<std::byte const> b, felspar::source_location loc) {
-    auto sqe = ring->next_sqe();
-    auto *c = new completion{this};
-    ::io_uring_prep_write(sqe, fd, b.data(), b.size(), 0);
-    ::io_uring_sqe_set_data(sqe, c);
+    auto *c = new completion{this, [](completion *c) {
+                                 auto sqe = c->self->ring->next_sqe();
+                                 ::io_uring_prep_write(
+                                         sqe, c->fd, c->cbytes.data(),
+                                         c->cbytes.size(), 0);
+                                 ::io_uring_sqe_set_data(sqe, c);
+                             }};
+    c->fd = fd;
+    c->cbytes = b;
     return {c};
 }
 
 
 felspar::poll::iop<int> felspar::poll::io_uring_warden::accept(
         int fd, felspar::source_location loc) {
-    auto sqe = ring->next_sqe();
-    auto *c = new completion{this};
-    ::io_uring_prep_accept(sqe, fd, &c->addr, &c->addrlen, 0);
-    ::io_uring_sqe_set_data(sqe, c);
+    auto *c = new completion{this, [](completion *c) {
+                                 auto sqe = c->self->ring->next_sqe();
+                                 ::io_uring_prep_accept(
+                                         sqe, c->fd, &c->addr, &c->addrlen, 0);
+                                 ::io_uring_sqe_set_data(sqe, c);
+                             }};
+    c->fd = fd;
     return {c};
 }
 
@@ -38,29 +51,38 @@ felspar::poll::iop<void> felspar::poll::io_uring_warden::connect(
         sockaddr const *addr,
         socklen_t addrlen,
         felspar::source_location loc) {
-    auto sqe = ring->next_sqe();
-    auto *c = new completion{this};
-    ::io_uring_prep_connect(sqe, fd, addr, addrlen);
-    ::io_uring_sqe_set_data(sqe, c);
+    auto *c = new completion{this, [](completion *c) {
+                                 auto sqe = c->self->ring->next_sqe();
+                                 ::io_uring_prep_connect(
+                                         sqe, c->fd, c->addrptr, c->addrlen);
+                                 ::io_uring_sqe_set_data(sqe, c);
+                             }};
+    c->fd = fd;
+    c->addrptr = addr;
+    c->addrlen = addrlen;
     return {c};
 }
 
 
 felspar::poll::iop<void> felspar::poll::io_uring_warden::read_ready(
         int fd, felspar::source_location loc) {
-    auto sqe = ring->next_sqe();
-    auto *c = new completion{this};
-    ::io_uring_prep_poll_add(sqe, fd, POLLIN);
-    ::io_uring_sqe_set_data(sqe, c);
+    auto *c = new completion{this, [](completion *c) {
+                                 auto sqe = c->self->ring->next_sqe();
+                                 ::io_uring_prep_poll_add(sqe, c->fd, POLLIN);
+                                 ::io_uring_sqe_set_data(sqe, c);
+                             }};
+    c->fd = fd;
     return {c};
 }
 
 
 felspar::poll::iop<void> felspar::poll::io_uring_warden::write_ready(
         int fd, felspar::source_location loc) {
-    auto sqe = ring->next_sqe();
-    auto *c = new completion{this};
-    ::io_uring_prep_poll_add(sqe, fd, POLLOUT);
-    ::io_uring_sqe_set_data(sqe, c);
+    auto *c = new completion{this, [](completion *c) {
+                                 auto sqe = c->self->ring->next_sqe();
+                                 ::io_uring_prep_poll_add(sqe, c->fd, POLLOUT);
+                                 ::io_uring_sqe_set_data(sqe, c);
+                             }};
+    c->fd = fd;
     return {c};
 }
