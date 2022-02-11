f0f9fc6aad1163546c6b7bb316f5446016a7a8fc
Kirit Sælensminde <kirit@kirit.com>
2022-02-11 17:30:32 +0700

    This should really be a system error (using the POSIX error number)

--- include/felspar/io/exceptions.hpp
+++ include/felspar/io/exceptions.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
 
-#include <felspar/exceptions/messaging_error.hpp>
+#include <felspar/exceptions/system_error.hpp>
 
 
 namespace felspar::io {
 
 
     /// Thrown when an IOP times out
-    class timeout : public exceptions::messaging_error<std::runtime_error> {
-        using superclass = exceptions::messaging_error<std::runtime_error>;
+    class timeout : public stdexcept::system_error {
+        using superclass = stdexcept::system_error;
 
       public:
-        using superclass::messaging_error;
+        using superclass::system_error;
     };
 
 
