2fb2203cdc8a60ec4ad8592c7a0606137d0b57d3
Kirit Sælensminde <kirit@kirit.com>
2022-03-20 12:45:41 +0700

    Start to add code to run server in a remote thread.

--- examples/echo-benchmark.cpp
+++ examples/echo-benchmark.cpp
@@ -1,6 +1,10 @@
 #include <felspar/io.hpp>
 #include <felspar/coro/start.hpp>
 
+#include <atomic>
+#include <iostream>
+#include <thread>
+
 
 using namespace std::literals;
 
@@ -8,20 +12,26 @@ using namespace std::literals;
 namespace {
 
 
+    std::uint16_t g_port = 2556;
+    std::atomic<std::uint64_t> server_count_started{}, server_count_completed{};
+
+
     felspar::coro::task<void> echo_connection(
             felspar::io::warden &ward, felspar::posix::fd sock) {
+        ++server_count_started;
         std::array<std::byte, 256> buffer;
-        while (auto bytes = co_await ward.read_some(sock, buffer, 20ms)) {
+        while (auto bytes = co_await felspar::io::ec(
+                       ward.read_some(sock, buffer, 20ms))) {
             std::span writing{buffer};
             auto written = co_await felspar::io::write_all(
-                    ward, sock, writing.first(bytes), 20ms);
+                    ward, sock, writing.first(bytes.value()), 20ms);
         }
+        ++server_count_completed;
     }
-    felspar::coro::task<void>
-            echo_server(felspar::io::warden &ward, std::uint16_t port) {
+    felspar::coro::task<void> echo_server(felspar::io::warden &ward) {
         auto fd = ward.create_socket(AF_INET, SOCK_STREAM, 0);
         felspar::posix::set_reuse_port(fd);
-        felspar::posix::bind_to_any_address(fd, port);
+        felspar::posix::bind_to_any_address(fd, g_port);
 
         int constexpr backlog = 64;
         if (::listen(fd.native_handle(), backlog) == -1) {
@@ -36,15 +46,24 @@ namespace {
             co.gc();
         }
     }
+    felspar::coro::task<void> server_manager(
+            felspar::io::warden &ward, felspar::posix::fd control) {
+        felspar::coro::starter<felspar::coro::task<void>> server;
+        server.post(echo_server, std::ref(ward));
+        std::cout << "Echo server running, waiting for end signal "
+                  << control.native_handle() << std::endl;
+        std::array<std::byte, 8> buffer;
+        co_await ward.read_some(control, buffer);
+        std::cout << "Server has seen end signal, terminating" << std::endl;
+    }
 
 
-    felspar::coro::task<void>
-            client(felspar::io::warden &ward, std::uint16_t port) {
+    felspar::coro::task<void> client(felspar::io::warden &ward) {
         auto fd = ward.create_socket(AF_INET, SOCK_STREAM, 0);
 
         sockaddr_in in;
         in.sin_family = AF_INET;
-        in.sin_port = htons(port);
+        in.sin_port = htons(g_port);
         in.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
         co_await ward.connect(
@@ -58,10 +77,23 @@ namespace {
 
 
     felspar::coro::task<int> co_main(felspar::io::warden &ward) {
-        felspar::coro::starter<felspar::coro::task<void>> server;
-        server.post(echo_server, std::ref(ward), 2566);
+        auto control_pipe = ward.create_pipe();
+        std::thread server{
+                [](felspar::posix::fd control) {
+                    felspar::io::poll_warden ward;
+                    ward.run(server_manager, std::move(control));
+                },
+                std::move(control_pipe.read)};
         co_await ward.sleep(2s);
-        co_await client(ward, 2566);
+        co_await client(ward);
+        std::cout << "Test done, signalling server to stop "
+                  << control_pipe.write.native_handle() << std::endl;
+        std::array<std::byte, 1> signal{std::byte{'x'}};
+        co_await ward.write_some(control_pipe.write, signal);
+        std::cout << "Waiting for server thread to end" << std::endl;
+        server.join();
+        std::cout << "Server count started " << server_count_started
+                  << " completed " << server_count_completed << '\n';
         co_return 0;
     }
 
