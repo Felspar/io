7186c5937b3a452beb3311ebbc98401a119a699d
Kirit Sælensminde <kirit@kirit.com>
2022-07-31 22:08:25 +0700

    Initial read buffer that allows us to read the HTTP request from the network

--- examples/http-simple.cpp
+++ examples/http-simple.cpp
@@ -1,11 +1,36 @@
 #include <felspar/coro/start.hpp>
 #include <felspar/io.hpp>
+#include <felspar/memory/hexdump.hpp>
 
 #include <iostream>
 
 
 namespace {
 
+    /**
+     * ## HTTP requests
+     *
+     * Processes HTTP requests on the connection.
+     */
+    felspar::coro::task<void>
+            http_request(felspar::io::warden &ward, felspar::posix::fd fd) {
+        std::cout << "Got HTTP request\n";
+        felspar::io::read_buffer<std::array<char, 2 << 10>> buffer;
+        auto const request{
+                co_await felspar::io::read_until_lf_strip_cr(ward, fd, buffer)};
+        std::cout << "Request: " << felspar::memory::hexdump(request);
+        for (auto header = co_await felspar::io::read_until_lf_strip_cr(
+                     ward, fd, buffer);
+             header.size();
+             header = co_await felspar::io::read_until_lf_strip_cr(
+                     ward, fd, buffer)) {
+            std::cout << felspar::memory::hexdump(header);
+        }
+        /// TODO Send response
+        co_await ward.close(std::move(fd));
+        std::cout << "And we're done\n";
+        co_return;
+    }
 
     /**
      * ## Accept loop
@@ -17,6 +42,7 @@ namespace {
         felspar::coro::starter<felspar::coro::task<void>> co;
         for (auto connections = felspar::io::accept(ward, socket);
              auto cnx = co_await connections.next();) {
+            co_await http_request(ward, felspar::posix::fd{*cnx});
             co.garbage_collect_completed();
         }
     }
@@ -26,7 +52,7 @@ namespace {
      * ## Main
      */
     felspar::coro::task<int> co_main(felspar::io::warden &ward) {
-        constexpr std::uint16_t port{61789};
+        constexpr std::uint16_t port{4040};
         constexpr int backlog = 64;
 
         /// Create the accept socket
--- include/felspar/io/read.hpp
+++ include/felspar/io/read.hpp
@@ -12,6 +12,61 @@
 namespace felspar::io {
 
 
+    /// A read buffer that can be split up and have more information read into
+    /// it over time as data appears on the file descriptor
+    template<typename R>
+    class read_buffer {
+        R storage = {};
+        std::span<typename R::value_type> data_read = {storage.data(), {}};
+        std::span<std::byte> empty_buffer = {
+                reinterpret_cast<std::byte *>(storage.data()), storage.size()};
+
+      public:
+        using storage_type = R;
+        using span_type = decltype(data_read);
+        using value_type = typename R::value_type;
+
+        read_buffer() {}
+
+        template<typename S>
+        felspar::coro::task<void> read_some(
+                warden &ward,
+                S &&sock,
+                std::optional<std::chrono::nanoseconds> timeout = {},
+                felspar::source_location const &loc =
+                        felspar::source_location::current()) {
+            std::size_t const bytes_read =
+                    co_await ward.read_some(sock, remaining(), timeout, loc);
+            if (bytes_read == 0) {
+                throw stdexcept::runtime_error{
+                        "Got a zero read, we're done", loc};
+            } else {
+                data_read = {data_read.data(), data_read.size() + bytes_read};
+                empty_buffer = empty_buffer.subspan(bytes_read);
+            }
+        }
+
+        span_type consume(std::size_t const bytes) {
+            auto const start = data_read.first(bytes);
+            data_read = data_read.subspan(bytes);
+            return start;
+        }
+
+        /// TODO Almost certainly remove these
+        auto not_consumed() const { return data_read; }
+        std::span<std::byte> remaining() {
+            return {reinterpret_cast<std::byte *>(empty_buffer.data()),
+                    empty_buffer.size()};
+        }
+        auto data() { return storage.data(); }
+        auto find(value_type v) {
+            return std::find(data_read.begin(), data_read.end(), v);
+        }
+        auto begin() { return data_read.begin(); }
+        auto end() { return data_read.end(); }
+    };
+
+
     /// Issue a read request for a specific amount of data
     template<typename S>
     inline felspar::coro::task<std::size_t> read_exactly(
@@ -56,6 +111,30 @@ namespace felspar::io {
                 std::span<std::byte>{
                         reinterpret_cast<std::byte *>(b.data()), b.size()},
                 std::move(timeout), loc);
+    }
+
+
+    /// Read a line (up to the next LF) and strip any final CR
+    template<typename S, typename R>
+    inline felspar::coro::task<typename R::span_type> read_until_lf_strip_cr(
+            warden &ward,
+            S &&sock,
+            R &read_buffer,
+            std::optional<std::chrono::nanoseconds> timeout = {},
+            felspar::source_location const &loc =
+                    felspar::source_location::current()) {
+        auto cr = read_buffer.find('\n');
+        while (cr == read_buffer.end()) {
+            co_await read_buffer.read_some(ward, sock, timeout, loc);
+            cr = read_buffer.find('\n');
+        }
+        std::size_t const line = std::distance(read_buffer.begin(), cr);
+        auto read = read_buffer.consume(line + 1).first(line);
+        if (read.size() and read.back() == '\r') {
+            co_return read.first(read.size() - 1);
+        } else {
+            co_return read;
+        }
     }
 
 
