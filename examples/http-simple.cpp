b397a0d47dc758b6a4ae28d6f864cfd2ff1371ed
Kirit Sælensminde <kirit@kirit.com>
2022-09-09 23:04:33 +0700

    Abstract out the response size

--- examples/http-simple.cpp
+++ examples/http-simple.cpp
@@ -1,18 +1,27 @@
 #include <felspar/io.hpp>
 #include <felspar/memory/hexdump.hpp>
 
+#include <fstream>
 #include <iostream>
 
 
 namespace {
+
+
+    /// HTTP response options
+    std::span<std::byte const> short_text();
+    std::span<std::byte const> big_octets();
+
 
     /**
      * ## HTTP requests
      *
      * Processes HTTP requests on the connection.
      */
-    felspar::io::warden::task<void>
-            http_request(felspar::io::warden &ward, felspar::posix::fd fd) {
+    felspar::io::warden::task<void> http_request(
+            felspar::io::warden &ward,
+            felspar::posix::fd fd,
+            std::span<std::byte const> const response) {
         felspar::io::read_buffer<std::array<char, 2 << 10>> buffer;
         auto const request{
                 co_await felspar::io::read_until_lf_strip_cr(ward, fd, buffer)};
@@ -24,12 +33,7 @@ namespace {
             /// The headers should really be processed
         }
         /// TODO Send response
-        co_await write_all(
-                ward, fd,
-                std::string_view{"HTTP/1.0 200 OK\r\n"
-                                 "Content-Length: 3\r\n"
-                                 "\r\n"
-                                 "OK\n"});
+        co_await write_all(ward, fd, response);
         co_await ward.close(std::move(fd));
         co_return;
     }
@@ -44,7 +48,8 @@ namespace {
         felspar::io::warden::starter<void> co;
         for (auto connections = felspar::io::accept(ward, socket);
              auto cnx = co_await connections.next();) {
-            co.post(http_request, std::ref(ward), felspar::posix::fd{*cnx});
+            co.post(http_request, std::ref(ward), felspar::posix::fd{*cnx},
+                    big_octets());
             co.garbage_collect_completed();
         }
     }
@@ -83,4 +88,36 @@ int main() {
         std::cerr << "Caught an exception: " << e.what() << '\n';
         return -1;
     }
+}
+
+
+namespace {
+
+
+    std::span<std::byte const> short_text() {
+        constexpr std::string_view sv{
+                "HTTP/1.0 200 OK\r\n"
+                "Content-Length: 3\r\n"
+                "\r\n"
+                "OK\n"};
+        return {reinterpret_cast<std::byte const *>(sv.data()), sv.size()};
+    }
+
+    constexpr std::string_view prefix{
+            "HTTP/1.0 200 OK\r\n"
+            "Content-Length: "};
+    auto const big{[]() {
+        std::string buffer{prefix};
+        constexpr std::size_t bytes{10 << 10};
+        buffer += std::to_string(bytes);
+        buffer += "\r\n\r\n";
+        std::vector<char> data(bytes);
+        std::ifstream{"/dev/urandom"}.read(data.data(), bytes);
+        buffer += std::string_view{data.data(), bytes};
+        return buffer;
+    }()};
+    std::span<std::byte const> big_octets() {
+        return {reinterpret_cast<std::byte const *>(big.data()), big.size()};
+    }
+
 }
