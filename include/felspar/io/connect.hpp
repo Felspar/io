3eaee68d6912c3fd30c0e2d5d9231ec099b92ab1
Kirit Sælensminde <kirit@felspar.com>
2024-03-15 14:11:03 +0700

    Add a connect function

--- include/felspar/io/connect.hpp
+++ include/felspar/io/connect.hpp
@@ -1,4 +1,24 @@
 #pragma once
 
 
-namespace felspar::io {}
+#include <felspar/io/warden.hpp>
+
+
+namespace felspar::io {
+
+
+    template<typename Socket>
+    inline auto
+            connect(warden &ward,
+                    Socket &&sock,
+                    sockaddr const *const addr,
+                    socklen_t const addrlen,
+                    std::optional<std::chrono::nanoseconds> const timeout = {},
+                    felspar::source_location const &loc =
+                            felspar::source_location::current()) {
+        return ward.connect(
+                std::forward<Socket>(sock), addr, addrlen, timeout, loc);
+    }
+
+
+}
