4ce3059d85ba85f9a75d02d025bf61dd686fa565
Kirit Sælensminde <kirit@felspar.com>
2026-06-13 15:58:16 +0700

    Add an overload for tasks returning the warden's task type

--- include/felspar/io/warden.hpp
+++ include/felspar/io/warden.hpp
@@ -50,6 +50,13 @@ namespace felspar::io {
             run_until(handle.get());
             return handle.promise().consume_value();
         }
+        template<typename Ret, typename N, typename... PArgs, typename... MArgs>
+        Ret run(N &o, task<Ret> (N::*f)(warden &, PArgs...), MArgs &&...margs) {
+            auto task = (o.*f)(*this, std::forward<MArgs>(margs)...);
+            auto handle = task.release();
+            run_until(handle.get());
+            return handle.promise().consume_value();
+        }
 
         /// Run a single IO submission and resume ready coroutines. Only
         /// processing is performed without any waits
